a85c038c523d3c7fbd19d922f3afee84c4ea1c13
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-02-03 22:35:30 +0200

    clean up subscription resolvers formatting

--- schema/resolvers/Subscription/researchJobStatus.ts
+++ schema/resolvers/Subscription/researchJobStatus.ts
@@ -1,7 +1,8 @@
-
-        import type   { SubscriptionResolvers } from './../../types.generated';
-        export const researchJobStatus: NonNullable<SubscriptionResolvers['researchJobStatus']> = {
-      subscribe: async (_parent, _arg, _ctx) => { 
-        throw new Error("Subscription.researchJobStatus not implemented");
-      },
-    }
+import type { SubscriptionResolvers } from "./../../types.generated";
+export const researchJobStatus: NonNullable<
+  SubscriptionResolvers["researchJobStatus"]
+> = {
+  subscribe: async (_parent, _arg, _ctx) => {
+    throw new Error("Subscription.researchJobStatus not implemented");
+  },
+};
